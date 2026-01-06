6c7b28775db1580b751e0882139cc0d7f3a9592c
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-01-06 16:46:03 -0500

    update

--- inventory-service/src/middleware/auth.middleware.js
+++ inventory-service/src/middleware/auth.middleware.js
@@ -3,7 +3,10 @@ import jwt from 'jsonwebtoken';
 export const verifyToken = (req, res, next) => {
     // 1. Intentar obtener el token de las cookies
     const token = req.cookies?.auth_token;
-    console.log(req.cookies);
+    
+    console.log('Headers recibidos:', req.headers);
+    console.log('Cookies recibidas:', req.cookies);
+
     if (!token) {
         return res.status(401).json({
             message: 'Acceso denegado. No se encontró un token de sesión.'
