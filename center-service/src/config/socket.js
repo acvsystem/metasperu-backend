b287e39f17d3aca4f2a361a406669b4858bf422a
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-02-18 15:01:52 -0500

    test

--- center-service/src/config/socket.js
+++ center-service/src/config/socket.js
@@ -28,7 +28,8 @@ export const initSocket = (server) => {
         socket.on('registrar_dashboard', () => {
             socket.join('dashboards');
             // Enviamos solo a ESTE socket la lista actual de tiendas
-            tiendasOnline.push(Object.values(tiendasActivas));
+            (tiendasOnline || []).push(Object.values(tiendasActivas));
+
             socket.emit('actualizar_dashboard', Object.values(tiendasActivas));
             console.log('Dashboard refrescado y sincronizado');
         });
