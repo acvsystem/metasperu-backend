89fca029ead1b9d694f23aed9bce6f11c3d5039a
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-02-20 09:33:02 -0500

    test

--- center-service/src/controllers/configuration.controller.js
+++ center-service/src/controllers/configuration.controller.js
@@ -4,17 +4,17 @@ export const configurationController = {
 
     postParametersStore: async (req, res) => {
         const { serie, mac } = req.body;
-        console.log(serie, mac);
+
         try {
             const [parameters] = await pool.execute(`SELECT * FROM TB_PARAMETROS_TIENDA WHERE MAC='${mac}).toUpperCase()}';`);
-
+            console.log(parameters);
             if (parameters.length) {
                 const [sqlTraffics] = await pool.query(`SELECT IP FROM tb_traffic_counter_tienda WHERE CODIGO_TIENDA = '${serie}';`) || [];
 
                 const traffics = sqlTraffics.map(t => {
                     return t.IP;
                 });
-                
+
                 parameters[0]['TRAFFIC_COUNTERS'] = traffics;
             }
 
