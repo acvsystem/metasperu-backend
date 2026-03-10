71b4b7fcefbe6dd24c575ea89da1b9440a454ce9
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-03-10 17:07:14 -0500

    test

--- center-service/src/controllers/store.controller.js
+++ center-service/src/controllers/store.controller.js
@@ -116,12 +116,19 @@ export const storeController = {
         const { socketId } = req.params;
 
         emailService.pushToEmailQueue({
-            email:'andrecanalesv@gmail.com',
-            subject: 'Bienvenido',
-            template: 'welcome',
-            variables: { name: 'Andrés Canales' }
+            email: 'andrecanalesv@gmail.com',
+            subject: 'Documentos Pendientes - BBW JOCKEY',
+            template: 'documentosPendientes',
+            variables: {
+                tienda: 'BBW JOCKEY', // Esta es la variable {{tienda}}
+                documentos: [ // Esta es la lista para el {{#each documentos}}
+                    { id: 'B7F1-00093991', tipo: 'Factura Electrónica', fecha: '2025-10-21' },
+                    { id: 'B7F1-00093992', tipo: 'Boleta de Venta', fecha: '2025-10-22' },
+                    { id: 'C2F1-00010001', tipo: 'Guía de Remisión', fecha: '2025-11-01' }
+                ]
+            }
         });
-        
+
         try {
             let onlineStore = Object.values(tiendasOnline);
             console.log("callDocumentsComparation onlineStore:", onlineStore);
