b6ab9086ddfd1d6c1de01073b3c2b30571d9044d
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-03-11 15:56:07 -0500

    test

--- center-inventory-service/src/routes/inventory.routes.js
+++ center-inventory-service/src/routes/inventory.routes.js
@@ -5,7 +5,7 @@ import { storeController } from '../controllers/store.controller.js';
 const router = Router();
 
 // --- RUTAS PARA INVENTARIO TIENDAS
-router.post('/api/inventory/store', verifyToken, storeController.postReqInventory);
-router.get('/api/inventory/store', verifyToken, storeController.callInventoryStore);
+router.post('/api/inventory/store', storeController.postReqInventory);
+router.get('/api/inventory/store', storeController.callInventoryStore);
 
 export default router;
