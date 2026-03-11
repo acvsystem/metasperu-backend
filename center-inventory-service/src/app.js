5ab728ade94fbd800215235c46efd55771667599
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-03-11 15:53:48 -0500

    update

--- center-inventory-service/src/app.js
+++ center-inventory-service/src/app.js
@@ -2,7 +2,6 @@
 
 import express from 'express';
 import { createServer } from 'http';
-import cookieParser from 'cookie-parser';
 import { initSocket } from './config/socket.js'; // 1. Importar primero
 import cors from 'cors';
 
@@ -16,7 +15,6 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
