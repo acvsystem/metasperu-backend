64d3c583c04e2ce77bc8a7caa15ad97e9c494664
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-03-20 15:17:53 -0500

    test

--- center-resources-human-service/src/controllers/store.controller.js
+++ center-resources-human-service/src/controllers/store.controller.js
@@ -175,7 +175,7 @@ const searchPapeletaEmpleado = async (fecha, documento) => {
             SELECT CODIGO_PAPELETA FROM TB_HEAD_PAPELETA 
             WHERE ID_PAP_TIPO_PAPELETA = 7 AND NRO_DOCUMENTO_EMPLEADO = ? AND FECHA_DESDE = ?
         `;
-       
+
         const [rows] = await pool.query(query, [documento.trim(), fecha]);
 
         if (rows && rows.length > 0) {
@@ -293,6 +293,7 @@ const procesarAsistenciaFinal = async (empleados, marcaciones) => {
         return {
             property: emp.CODEJB ? emp.CODEJB.trim() : dni,
             data: asistenciaDiaria
+
         };
     }));
 
