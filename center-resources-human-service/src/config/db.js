55ef374131b457468e3396df8cc1a6ee850e9b76
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-03-20 11:14:28 -0500

    test

--- center-resources-human-service/src/config/db.js
+++ center-resources-human-service/src/config/db.js
@@ -9,7 +9,7 @@ export const pool = mysql.createPool({
     host: process.env.DB_HOST || '192.168.0.9',
     user: process.env.DB_USER || 'dbserver',
     password: process.env.DB_PASSWORD || 'J4s0nd34d$$',
-    database: process.env.DB_NAME || 'metasperu_bd',
+    database: process.env.DB_NAME || 'bd_metasperu',
     waitForConnections: true,
     connectionLimit: 10, // Máximo de conexiones simultáneas
     queueLimit: 0
