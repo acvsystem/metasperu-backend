7f6610b1ff37451256d728d486be27d630f5ff8e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-04-10 10:57:25 -0500

    test

--- center-inventory-service/src/controllers/store.controller.js
+++ center-inventory-service/src/controllers/store.controller.js
@@ -167,7 +167,7 @@ export const storeController = {
 
             // 3. Subida al FTP
             // Usamos la ruta dinámica que viene en el body o la de prueba
-            const targetDir = `ITPERU/PRUEBA`;
+            const targetDir = `ITPERU/${rutaDirectory}` || `ITPERU/PRUEBA`;
             await client.ensureDir(targetDir);
             await client.uploadFrom(filePath, fileName);
 
