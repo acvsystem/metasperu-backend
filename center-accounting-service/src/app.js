1649812d7cd026bf2091ca96b59fa4ec02188db1
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-04-15 17:18:16 -0500

    test

--- center-accounting-service/src/app.js
+++ center-accounting-service/src/app.js
@@ -31,7 +31,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- AUTOMATIZACIÓN: CRON JOB A LAS 9:00 AM ---
-cron.schedule('18 17 * * *', async () => {
+cron.schedule('19 17 * * *', async () => {
   console.log('⏰ [Cron Job] Iniciando comprobación diaria de Tipo de Cambio...');
 
   const fechaHoy = new Date().toISOString().split('T')[0];
@@ -70,7 +70,7 @@ cron.schedule('18 17 * * *', async () => {
         io.to('7A').emit('py_request_exchange_rate', { pedido_por: 'cron_accounting', init: fechaHoy, end: fechaHoy });
       }
     } else {
-      console.log(`✅ [Cron] El tipo de cambio para hoy (${fechaHoy}) ya existe en la DB local.${rows}`);
+      console.log(`✅ [Cron] El tipo de cambio para hoy (${fechaHoy}) ya existe en la DB local.${rows[0]}`);
 
       await extraServices.enviarSlack(`⚠️ [Cron] El tipo de cambio para hoy (${fechaHoy}), Venta: ${rows[0].venta} ya existe en la DB local.`, "Monitor de Tipo de Cambio");
 
