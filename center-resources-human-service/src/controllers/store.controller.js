9083226d1188c49420e1ba9f01bf405fc9e8787c
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-05-04 11:22:55 -0500

    test

--- center-resources-human-service/src/controllers/store.controller.js
+++ center-resources-human-service/src/controllers/store.controller.js
@@ -1545,7 +1545,7 @@ const procesarYRegistrarHoras = async (listaRegistros) => {
 
 
             // Convertimos hrWorking (decimal) a minutos enteros redondeados
-            console.log(reg.dia, reg.hrWorking);
+          
             const minutos = calcularDiferenciaMinutos(reg.hrIn, reg.hrOut);
             const esPartTime = reg.tpAsociado === '**';
             const esTurnoEspecial = reg.hrOut === '23:59:59' || reg.hrIn === '00:00:00';
@@ -1577,7 +1577,7 @@ const procesarYRegistrarHoras = async (listaRegistros) => {
         const papeletaRaw = await hrPapeleta(fecha, data.nroDocumento);
         const minsPapeleta = Math.round(tiempoADecimal(papeletaRaw.horas) * 60);
 
-        console.log(minutosAHoras(data.totalMins));
+        console.log(data.totalMins);
         // Suma total efectiva en minutos (Exacta)
         const totalMinsEfectivos = data.totalMins + minsPapeleta;
 
