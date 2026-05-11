e975b200e4ac5cf8d2607254e43a221eb98e2211
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-05-11 10:35:56 -0500

    configuration zoho

--- email-service/src/mailer.js
+++ email-service/src/mailer.js
@@ -1,25 +1,24 @@
 import nodemailer from 'nodemailer';
-import hbs from 'nodemailer-express-handlebars'; // <-- Cambiado de require a import
+import hbs from 'nodemailer-express-handlebars';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// --- CONFIGURACIÓN PARA ZOHO ---
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    port: 465,
-    secure: true,
+    host: "smtppro.zoho.com", // Servidor SMTP de Zoho para cuentas profesionales
+    port: 465,                // Puerto para SSL
+    secure: true,             // true para puerto 465
     auth: {
-        user: 'itperu.notification@gmail.com',
-        pass: 'fxmcqlzirhrkihzx'
+        user: 'notificacion@metasperu.net.pe', // Tu nuevo correo de Zoho
+        pass: 'J4s0nd34d$$'  // La contraseña de aplicación generada
     }
-})
+});
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
 const viewPath = path.resolve(__dirname, './templates/');
 
-
-console.log(viewPath);
-// Configurar plantillas
+// Configurar plantillas (se mantiene igual)
 transporter.use('compile', hbs({
     viewEngine: {
         partialsDir: viewPath,
@@ -32,7 +31,7 @@ transporter.use('compile', hbs({
 export const mailer = {
     sendMail: async (to, subject, template, context, archivo = null) => {
         let mailOptions = {
-            from: '"Metas Perú" <itperu@metasperu.com>', // Usamos el correo autenticado para evitar spam
+            from: '"Metas Perú" <notificacion@metasperu.net.pe>', // DEBE coincidir con el user de auth
             to: to,
             subject: subject,
             template: template,
@@ -40,24 +39,22 @@ export const mailer = {
             attachments: []
         };
 
-        // Si existe un archivo, lo añadimos al array de adjuntos
         if (archivo && archivo.content) {
             mailOptions.attachments.push({
                 filename: archivo.filename,
                 content: archivo.content,
-                encoding: 'base64', // <--- IMPORTANTE: Nodemailer sabrá qué hacer
+                encoding: 'base64',
                 contentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
             });
         }
 
         try {
             const info = await transporter.sendMail(mailOptions);
-            console.log("Email enviado con éxito: %s", info.messageId);
+            console.log("Email enviado vía Zoho: %s", info.messageId);
             return info;
         } catch (error) {
-            console.error("Error al enviar email:", error);
+            console.error("Error al enviar email con Zoho:", error);
             throw error;
         }
     }
 }
-
