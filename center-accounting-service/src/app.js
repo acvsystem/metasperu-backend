541637dcee23ac6392b30fe0720a54887e5275c8
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-05-12 09:23:37 -0500

    test

--- center-accounting-service/src/app.js
+++ center-accounting-service/src/app.js
@@ -32,7 +32,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- AUTOMATIZACIÓN: CRON JOB A LAS 9:00 AM ---
-cron.schedule('44 08 * * *', async () => {
+cron.schedule('25 09 * * *', async () => {
   console.log('⏰ [Cron Job] Iniciando comprobación diaria de Tipo de Cambio...');
 
   // 1. Asegurar fecha correcta en Lima (evita desfases de UTC)
