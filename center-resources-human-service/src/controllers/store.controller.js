e346fb0d1a2e5fdb9f4e1530422d0c4398de43f2
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-05-14 16:32:54 -0500

    test

--- center-resources-human-service/src/controllers/store.controller.js
+++ center-resources-human-service/src/controllers/store.controller.js
@@ -606,7 +606,15 @@ export const storeController = {
              );`,
                             [codigoTienda, fechaIn, fechaFormateada]
                         );
-                        
+                        console.log(`SELECT ID_HEAD_PAPELETA, CODIGO_PAPELETA, NRO_DOCUMENTO_EMPLEADO, 
+                    DATE_FORMAT(FECHA_DESDE, '%d-%m-%Y') AS FECHA_DESDE, DESCRIPCION 
+             FROM bd_metasperu.tb_head_papeleta 
+             WHERE ID_PAP_TIPO_PAPELETA = 7 
+             AND CODIGO_TIENDA = '${codigoTienda}'
+             AND (
+                (FECHA_DESDE = ${fechaIn}) OR 
+                (DATE_FORMAT(FECHA_DESDE, '%d-%m-%Y') = ${fechaFormateada})
+             );`);
                         return paps; // Retorna el array de papeletas de este día
                     }));
 
