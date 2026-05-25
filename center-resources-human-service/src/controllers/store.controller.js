52eb387fe3e96b6f7bd948f6a4aa103211f5335c
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-05-24 20:07:22 -0500

    test

--- center-resources-human-service/src/controllers/store.controller.js
+++ center-resources-human-service/src/controllers/store.controller.js
@@ -123,7 +123,7 @@ export const storeController = {
             if (arDataAsistenciaEmpleados.length > 0) {
                 arDataAsistenciaEmpleados[0].ejb = empleadosUnicos;
             }
-
+            console.log(datosFormateados);
             // 4. Emitir al dashboard en tiempo real
             getIO().to(socketId).emit('dashboard_empleados_horario', datosFormateados);
 
