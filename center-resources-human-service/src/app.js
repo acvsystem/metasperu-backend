309069bbd241d6544d423d5b4104ab792d1fccaa
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-06-01 16:37:15 -0500

    test

--- center-resources-human-service/src/app.js
+++ center-resources-human-service/src/app.js
@@ -37,7 +37,7 @@ app.use(express.json({ limit: BODY_LIMIT }));
 app.use(express.urlencoded({ limit: BODY_LIMIT, extended: true }));
 
 
-cron.schedule('42 15 * * *', async () => {
+cron.schedule('0 8 * * 0', async () => {
   console.log('⏰ [Cron Job] Iniciando comprobación de horarios creados...');
 
   const hoy = new Date();
@@ -58,10 +58,10 @@ cron.schedule('42 15 * * *', async () => {
         FROM TB_LISTA_TIENDA LT
         LEFT JOIN TB_HORARIO_PROPERTY HP
             ON HP.CODIGO_TIENDA = LT.SERIE_TIENDA
-            AND TRIM(SUBSTRING_INDEX(HP.RANGO_DIAS, ' ', 1)) = ?
-        WHERE HP.CODIGO_TIENDA IS NULL;`;
+            AND TRIM(SUBSTRING_INDEX(HP.RANGO_DIAS, ' ', 1)) = '01-06-2026'
+        WHERE HP.CODIGO_TIENDA IS NULL AND LT.SERIE_TIENDA != '9M' AND LT.ESTATUS != 'INACTIVO';`;
 
-    const [rows] = await pool.query(query, ['01-06-2026']);
+    const [rows] = await pool.query(query, [day]);
 
     if (rows.length > 0) {
       emailService.pushToEmailQueue({
@@ -75,7 +75,7 @@ cron.schedule('42 15 * * *', async () => {
         email: ['itperu@metasperu.com'],
         subject: `Alerta de tiendas Sin horario creado `,
         template: 'sedesHorarioCorrecto',
-        variables: { sedes: rows, periodo: day }
+        variables: { periodo: day }
       });
     }
 
