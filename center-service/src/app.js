342d1fa43dff2f7c9a37405c5463db2e93ea7b9e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-06-17 14:27:17 -0500

    update

--- center-service/src/app.js
+++ center-service/src/app.js
@@ -1,7 +1,6 @@
 
 
 import express from 'express';
-import { createServer } from 'http';
 import cookieParser from 'cookie-parser';
 import { initSocket } from './config/socket.js'; // 1. Importar primero
 import { storeController } from './controllers/store.controller.js';
@@ -24,12 +23,18 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 import centerRoutes from './routes/center.routes.js';
 app.use('/s1/center', centerRoutes);
 
-/*cron.schedule('00 8 * * 0', async () => {
+cron.schedule('28 14 * * 0', async () => {
   console.log('⏰ [Cron Job] Iniciando limpieza de clientes...');
   storeController.callClientDelete();
   try {
 
+    console.log('Iniciando tarea programada: callClientDelete...');
 
+    // Como el cron no viene de un HTTP request, le pasamos un ID por defecto 
+    // o un string que identifique que lo hizo el sistema.
+    const systemSocketId = 'SYSTEM_CRON';
+
+    executeClientDeleteLogic(systemSocketId);
 
   } catch (error) {
     const errorMsg = error.response?.data?.message || error.message;
@@ -38,7 +43,26 @@ app.use('/s1/center', centerRoutes);
 }, {
   scheduled: true,
   timezone: "America/Lima"
-});*/
+});
+
+
+const executeClientDeleteLogic = async (socketIdFallback) => {
+  try {
+    let [data] = await pool.query(`SELECT * FROM TB_CLIENTES_CLEAR_FORNT;`);
+    let extra_client = ((data || [])[0]['LIST_CLIENTE']).split(',');
+
+    if ((extra_client || []).length) {
+      // Asegúrate de que getIO() esté disponible en este archivo
+      getIO().to('grupo_tiendas').emit('py_delete_client', {
+        pedido_por: socketIdFallback,
+        extra_client: extra_client
+      });
+      console.log(`[CRON] Señal emitida exitosamente a las 10:00 AM`);
+    }
+  } catch (error) {
+    console.error('[CRON ERROR] Error en ejecución programada:', error);
+  }
+};
 
 const PORT = 3002;
 httpServer.listen(PORT, () => {
