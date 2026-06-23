283b2ce33ea70961c0f5b1b9c0a276f915985b4b
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-06-23 11:27:57 -0500

    test

--- center-resources-human-service/src/controllers/store.controller.js
+++ center-resources-human-service/src/controllers/store.controller.js
@@ -2153,8 +2153,11 @@ const procesarYRegistrarHoras = async (listaRegistros) => {
 
         // Conversión final a horas decimales para almacenamiento
         const excesoHorasFinal = Math.round((excesoMins / 60) * 100) / 100;
-        console.log(2156,excesoHorasFinal);
+        console.log(2156, excesoHorasFinal);
+        if (excesoHorasFinal >= 3.00) {
 
+        }
+        
         if (excesoHorasFinal >= MINIMO_PARA_REGISTRAR) {
             const result = await guardarEnBD(data.nroDocumento, fecha, excesoHorasFinal, observacion, esAprobacion);
             if (result.inserted) stats.insertados += 1;
