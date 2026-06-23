c1f475022308246245d253bbb243eeb7f8332755
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-06-23 11:35:08 -0500

    test

--- center-resources-human-service/src/controllers/store.controller.js
+++ center-resources-human-service/src/controllers/store.controller.js
@@ -2135,16 +2135,16 @@ const procesarYRegistrarHoras = async (listaRegistros) => {
             // Lógica de validación de marcaciones
             if (data.count === 1) {
                 observacion = tag + "Solo tiene 1 solo registro de marcacion";
-                esAprobacion = 1;
+                esAprobacion = 0;
             } else if (data.especial) {
                 observacion = tag + "No marco salida";
-                esAprobacion = 1;
+                esAprobacion = 0;
             } else if (data.count > 2) {
                 observacion = tag + "Marcacion irregular, verifique marcaciones.";
-                esAprobacion = 1;
+                esAprobacion = 0;
             } else if (nivel.nivel === 'RECURSOS HUMANOS') {
                 observacion = tag + "Tiene una papeleta ese dia.";
-                esAprobacion = 1;
+                esAprobacion = 0;
             } else if (data.lactancia) {
                 observacion = "Periodo de lactancia (Jornada reducida 7h).";
                 esAprobacion = 0;
@@ -2153,7 +2153,7 @@ const procesarYRegistrarHoras = async (listaRegistros) => {
 
         // Conversión final a horas decimales para almacenamiento
         const excesoHorasFinal = Math.round((excesoMins / 60) * 100) / 100;
-        console.log(2156, excesoHorasFinal);
+        console.log(2156, excesoHorasFinal,observacion);
         if (excesoHorasFinal >= 3.00 && observacion === null) {
             observacion = "Verificar marcaciones, exceso de 3 horas.";
             esAprobacion = 0;
