16a78c0b271c8206abfeb6f9923288218f2808a6
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-06-24 16:17:46 -0500

    test

--- center-inventory-service/src/controllers/store.controller.js
+++ center-inventory-service/src/controllers/store.controller.js
@@ -394,21 +394,7 @@ function actualizarMapaPorMarca(marca, serieStore, data, socketId) {
                 'marca': marca
             });
         } else {
-            mapaMarca.set(item.cCodigoBarra, {
-                'cCodigoArticulo': item.cCodigoArticulo,
-                'cReferencia': item.cReferencia,
-                'cCodigoBarra': item.cCodigoBarra,
-                'cDescripcion': item.cDescripcion,
-                'cDepartamento': item.cDepartamento,
-                'cSeccion': item.cSeccion,
-                'cFamilia': item.cFamilia,
-                'cSubFamilia': item.cSubFamilia,
-                'cTalla': item.cTalla,
-                'cColor': item.cColor,
-                'cTemporada': item.cTemporada,
-                'cStock': {}, // Aquí guardaremos los stocks de cada tienda
-                'marca': marca
-            });
+            mapaMarca.get(item.cCodigoBarra).cStock[serieStore] = 0;
         }
         // Asignamos el stock de la tienda específica
         mapaMarca.get(item.cCodigoBarra).cStock[serieStore] = item.cStock;
