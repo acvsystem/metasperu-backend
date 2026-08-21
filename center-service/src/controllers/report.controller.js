1e06a80f58b90b6fb85a7f0ccb36bbbdd8c4eb90
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-08-21 16:05:05 -0500

    test

--- center-service/src/controllers/report.controller.js
+++ center-service/src/controllers/report.controller.js
@@ -3,9 +3,9 @@ import { getIO } from '../config/socket.js';
 export const reportsController = {
 
     infRendimiento: async (req, res) => {
-        const { socket_id, fecha_desde, fecha_hasta } = req.params;
+        const { socket_id, fecha_desde, fecha_hasta } = req.query;
         try {
-            console.log('Emitir señal de informe de rendimiento', req.params);
+            console.log('Emitir señal de informe de rendimiento', req.query);
             getIO().to('grupo_tiendas').emit('py_request_informe_rendimiento', { pedido_por: socket_id, fecha_desde: fecha_desde, fecha_hasta: fecha_hasta });
 
             res.json({
