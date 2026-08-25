ca880c440f20641540c293458b6423260d152d06
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-08-25 11:14:31 -0500

    update

--- center-service/src/app.js
+++ center-service/src/app.js
@@ -122,7 +122,7 @@ cron.schedule('00 21 * * *', async () => {
   timezone: "America/Lima"
 });
 
-cron.schedule('56 10 * * *', async () => {
+cron.schedule('16 11 * * *', async () => {
   console.log('⏰ [Cron Job 3] Iniciando generacion de reportes de rendimiento...');
 
   // Fecha actual en formato YYYY-MM-DD (zona America/Lima)
